c2a79c24098e84c5a1d924b5644e42bf562e43fd
panwenfei <375358238@qq.com>
2017-12-23 15:19:10 +0800

    新增AWE项目

---
+++ AWE.PWF.WEB/AWE.PWF.WEB/Areas/BasicSetting/BasicSettingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AWE.PWF.WEB.Areas.BasicSetting
+{
+    public class BasicSettingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "BasicSetting";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "BasicSetting_default",
+                "BasicSetting/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
