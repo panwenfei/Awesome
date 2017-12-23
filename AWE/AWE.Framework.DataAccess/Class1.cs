c6add752f70d9fb1d3944be1cf88ae782a39f2fb
panwenfei <375358238@qq.com>
2017-12-23 17:56:29 +0800

    重命名

---
+++ AWE/AWE.Framework.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWE.Framework.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ AWE/AWE.PWF.WEB/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AWE.PWF.WEB
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
