ff9422c13328daf30d5a400e1bb9eeda01c5d8c3
panwenfei <375358238@qq.com>
2017-12-24 19:43:00 +0800

    添加局部分页

--- AWE/AWE.PWF.WEB/Controllers/HomeController.cs
+++ AWE/AWE.PWF.WEB/Controllers/HomeController.cs
@@ -8,22 +8,14 @@ namespace AWE.PWF.WEB.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Main()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
