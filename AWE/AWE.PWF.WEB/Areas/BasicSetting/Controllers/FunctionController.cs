0748a841917dc5c3b0fe3f75d21ecfc50351435d
panwenfei <375358238@qq.com>
2018-01-01 21:45:05 +0800

    规范样式

---
+++ AWE/AWE.PWF.WEB/Areas/BasicSetting/Controllers/FunctionController.cs
@@ -0,0 +1,48 @@
+﻿using AWE.PWF.Business;
+using AWE.PWF.Common;
+using AWE.PWF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AWE.PWF.WEB.Areas.BasicSetting.Controllers
+{
+    public class FunctionController : Controller
+    {
+        // GET: BasicSetting/Function
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult FunctionModal()
+        {
+            return View();
+        }
+
+        public ActionResult GetFunctionList()
+        {
+            try
+            {
+                DataTable dt = SystemSettingManager.GetFunctionList();
+                if (dt.Rows.Count > 0)
+                {
+                    //跳过dt的前10行，取后20行  即取得11-30行
+                    DataTable TakeTopList = dt.AsEnumerable().Skip((Convert.ToInt32(Request["page"]) - 1) * Convert.ToInt32(Request["limit"])).Take(Convert.ToInt32(Request["limit"])).CopyToDataTable<DataRow>();
+                    return Json(new { total = dt.Rows.Count, rows = ConvertHelper<ModelGoodsCheck>.ConvertToModel(TakeTopList).ToList() }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { total = dt.Rows.Count, rows = ConvertHelper<ModelGoodsCheck>.ConvertToModel(dt).ToList() }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { total = 0, rows = "" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
